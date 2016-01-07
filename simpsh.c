8a861973d1ced136c356fef088c58b267ceeb580
echan996 <echan96@g.ucla.edu>
2016-01-06 23:07:25 -0800

    Lab 1a
    Update option_parser

--- simpsh.c
+++ simpsh.c
@@ -1,13 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-void option_parser(const char ** argv, const int * values){
-	int i;
-	char* option = operation[values[0]];
-	if(option== "--rdonly"){
+#include <fcntl.h>
+int* open_files[100];
+int verbose = 0;
+void option_parser(const char ** argv,  int start, int length){
+	if (verbose){
+		for (int i = 0; i < length - 1; i++){
+			fprintf(stdout, argv[start + i] + ' ');
+		}
+		fprintf(stdout, argv[start + length - 1] + '\n');
 	}
-	else if (option == "--wronly"){}
-	else if (option=="--command"){}
-	else if (option == "--verbose"){}
+	int i;
+	char* option = argv[start];
+	if(option== "--rdonly"){
+		if (length != 2)
+			return;
+		int a = open(argv[start + 1], O_RDONLY);
+		if (a == -1){
+			fprintf(stderr, "Error: Unable to create file");
+			exit(1);
+		}
+		//some storage of the open value into some global int array.
+	}
+	else if (option == "--wronly"){
+		if (length != 2)
+			return;
+		int a = open(argv[start + 1], O_WRONLY);
+		if (a == -1){
+			fprintf(stderr, "Error: Unable to create file");
+			exit(1);
+		}
+	}
+	else if (option=="--command"){
+	
+	}
+	else if (option == "--verbose"){
+		verbose = 1;
+	}
 }
 
 int main(int argc, char **argv){
