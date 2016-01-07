6ebd665b13753df923d85abbea9189d3faebe892
echan996 <echan96@g.ucla.edu>
2016-01-06 22:28:22 -0800

    Lab 1a
    I'm still bad

--- simpsh.c
+++ simpsh.c
@@ -1,10 +1,22 @@
-void file_opening(char * option, char * file){
-	if(option== --rdonly"){
-
+#include <stdio.h>
+#include <stdlib.h>
+void option_parser(const char ** operation){
+	int i;
+	for (i = 0; option[i] != '\0' && option[i] != ' '; i++)
+		continue;
+	parse[i] = '\0';
+	if(parse== "--rdonly"){
+		int 
 	}
+	char** a = parse_option();
+		free a;
+	
 }
+
+
 int main(int argc, char **argv){
 	for (int i = 1; i < argc; i++){
-
+		//parse for options, eg find values in argv that begin with --
 	}
 }
+
