ad5e7efcbb281abea04ca8765fbb42f07ab0ccde
echan996 <echan96@g.ucla.edu>
2016-02-01 21:47:32 -0800

    Changed test.sh to make it more portable

--- test.sh
+++ test.sh
@@ -25,9 +25,11 @@ function should_succeed() {
     echo;
   fi
 }
-
-tmp_file=/tmp/foo
-tmp_file2=/tmp/foo2
+touch file1
+touch file2
+base64 /dev/urandom | head -c 50000000 > a
+tmp_file=file1
+tmp_file2=file2
 > "$tmp_file"
 > "$tmp_file2"
 
@@ -57,28 +59,28 @@ should_succeed "command reports malformed command options to user on stderr"
 
 
 ./simpsh \
-  --wronly /tmp/foo \
+  --wronly "$tmp_file" \
   --verbose \
   --command 0 0 0 echo foo \
   | grep "command 0 0 0 echo foo" > /dev/null
 should_succeed "command tracks all command options"
 
 
-./simpsh --wronly /tmp/foo --command 0 0 0 echo "foo";
-grep foo /tmp/foo > /dev/null;
+./simpsh --wronly "$tmp_file" --command 0 0 0 echo "foo";
+grep foo "$tmp_file" > /dev/null;
 should_succeed "command can write to write only file"
 
 
 > "$tmp_file"
 echo_path=$(which echo);
-./simpsh --wronly /tmp/foo --command 0 0 0 "$echo_path" "foo";
-grep foo /tmp/foo > /dev/null;
+./simpsh --wronly "$tmp_file" --command 0 0 0 "$echo_path" "foo";
+grep foo "$tmp_file" > /dev/null;
 should_succeed "path command can write to write only file"
 
 
 > "$tmp_file"
-./simpsh --rdonly /tmp/foo --command 0 0 0 echo "foo";
-grep foo /tmp/foo > /dev/null;
+./simpsh --rdonly "$tmp_file" --command 0 0 0 echo "foo";
+grep foo "$tmp_file" > /dev/null;
 # NOTE that failure of `echo "foo"` end up in stderr
 should_fail "shouldn't be able to write to read only file"
 
@@ -101,7 +103,7 @@ should_succeed "the temporary file should have 'foo'"
 cat "$tmp_file2" | grep "bar" > /dev/null
 should_succeed "the temporary file 2 should have 'bar'"
 
-# cat of /tmp/foo should end up in the /tmp/file2
+# cat of "$tmp_file" should end up in the /tmp/file2
 ./simpsh --rdonly "$tmp_file" --wronly "$tmp_file2" --command 0 1 0 cat "$tmp_file"
 cat "$tmp_file2" | grep "foo" > /dev/null && cat "$tmp_file2" | wc -l | grep 1 > /dev/null
 should_succeed "should be able to cat from one file to the other (replace bar with foo)"
