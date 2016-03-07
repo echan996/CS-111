6250cf8f8bd72fe73d3d144e81f2b814342dab80
echan996 <echan96@g.ucla.edu>
2016-03-06 23:29:00 -0800

    Fixed other stuf

--- Lab 4/slTest.c
+++ Lab 4/slTest.c
@@ -2,10 +2,10 @@
 #include "SortedList.c"
 
 static struct option long_options[] = {
-		{ "--threads", required_argument, 0, 'a' },
-		{ "--iter", required_argument, 0, 'b' },
-		{ "--iterations", required_argument, 0, 'b' },
-		{ "--yield", optional_argument, 0, 'c' },
+		{ "threads", required_argument, 0, 'a' },
+		{ "iter", required_argument, 0, 'b' },
+		{ "iterations", required_argument, 0, 'b' },
+		{ "yield", optional_argument, 0, 'c' },
 		{0,0,0,0}
 };
 
@@ -17,12 +17,12 @@ int main(int argc, char** argv){
 	while ((option = (char)getopt_long(argc, argv, "", long_options, &i)) != -1){
 		switch (option){
 		case 'a':
-			if (threads=atoi(optarg) == 0){
+			if ((threads=atoi(optarg)) == 0){
 				fprintf(stderr, "Error: Argument must be positive integer\n");
 			}
 			break;
 		case 'b':
-			if (iterations = atoi(optarg) == 0){
+			if ((iterations = atoi(optarg)) == 0){
 				fprintf(stderr, "Error: Argument must be positive integer\n");
 			}
 			break;
