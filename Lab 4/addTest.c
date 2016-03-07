9746a889467e77d2aa62ed2f702a2e0e9e27e078
echan996 <echan96@g.ucla.edu>
2016-03-06 20:46:42 -0800

    added longopt struct

--- Lab 4/addTest.c
+++ Lab 4/addTest.c
@@ -1,8 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <unistd.h>
 void add(long long *pointer, long long value) {
 	long long sum = *pointer + value;
 	*pointer = sum;
 }
 
-int main(){
-		
+static struct option long_options[] = {
+		{ "--threads", required_argument, 0, 'a' },
+		{ "--iter", required_argument, 0, 'b' },
+		{ "--iterations", required_argument, 0, 'b' },
+		{0,0,0,0}
+};
+
+
+int main(int argc, char** argv){
+	
 }
