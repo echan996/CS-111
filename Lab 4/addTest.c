aa2013997f57f64c2ef65b4a04f2925c059202ba
echan996 <echan96@g.ucla.edu>
2016-03-06 23:00:16 -0800

    bug fixes galore

--- Lab 4/addTest.c
+++ Lab 4/addTest.c
@@ -1,3 +1,4 @@
+#define _POSIX_C_SOURCE 199309L
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
@@ -13,16 +14,19 @@ void add(long long* pointer, long long value) {
 	*pointer = sum;
 }
 void* thread_action(void* arg){
-	add(&counter, 1);	
-	add(&counter, -1);
+	long long iterations = *(long long*)arg;
+	for (int i = 0; i < iterations;i++)
+		add(&counter, 1);	
+	for (int i = 0; i < iterations;i++)
+		add(&counter, -1);
 }
 
 
 struct timespec timer;
 static struct option long_options[] = {
-		{ "--threads", required_argument, 0, 'a' },
-		{ "--iter", required_argument, 0, 'b' },
-		{ "--iterations", required_argument, 0, 'b' },
+		{ "threads", required_argument, 0, 'a' },
+		{ "iter", required_argument, 0, 'b' },
+		{ "iterations", required_argument, 0, 'b' },
 		{0,0,0,0}
 };
 
@@ -38,12 +42,13 @@ int main(int argc, char** argv){
 	while ((option = (char)getopt_long(argc, argv, "", long_options, &i)) != -1){
 		switch (option){
 		case 'a':
-			if (threads = atoi(optarg) == 0){
+			if ((threads = atoi(optarg)) == 0){
 				fprintf(stderr, "Argument must be positive integer\n");
+				
 			}
 			break;
 		case 'b':
-			if (iterations = atoi(optarg) == 0){
+			if ((iterations = atoi(optarg)) == 0){
 				fprintf(stderr, "Argument must be positive integer\n");
 			}
 			break;
@@ -58,10 +63,10 @@ int main(int argc, char** argv){
 		fprintf(stderr, "Error: memory not allocated\n");
 		exit(1);
 	}
-	clock_gettime(CLOCK_MONOTONIC, timer);
+	clock_gettime(CLOCK_MONOTONIC, &timer);
 	time_init = timer.tv_sec * 1000000000 + timer.tv_nsec;
 	for (int a = 0, create_check = 0; a < threads; a++){
-		create_check = pthread_create(tids + a, 0, thread_action, &a);
+		create_check = pthread_create(tids + a, 0, thread_action, &iterations);
 		if (create_check){
 			fprintf(stderr, "Error: unable to create thread\n");
 		}
@@ -71,7 +76,7 @@ int main(int argc, char** argv){
 	}
 	
 	
-	clock_gettime(CLOCK_MONOTONIC, timer);
+	clock_gettime(CLOCK_MONOTONIC, &timer);
 	free(tids);
 	time_finish = timer.tv_sec * 1000000000 + timer.tv_nsec - time_init;
 	operations = threads* iterations * 2;
