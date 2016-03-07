023842fa84ee90864decd5b3a3f97f7f39b942f2
echan996 <echan96@g.ucla.edu>
2016-03-06 22:05:56 -0800

    Bug Fixes

--- Lab 4/addTest.c
+++ Lab 4/addTest.c
@@ -5,10 +5,10 @@
 #include <sys/time.h>
 #include <pthread.h>
 #include <sys/resource.h>
-typedef struct thread_info{
+typedef struct s_thread_info{
 	void* n_count;
 	int add_amount;
-};
+}thread_info;
 void add(long long* pointer, long long value) {
 	long long sum = *pointer + value;
 	*pointer = sum;
@@ -17,12 +17,12 @@ void thread_action(void* arg){
 	thread_info t = *(thread_info*)arg;
 	long long* pointer = t.n_count;
 	int value = t.add_amount;
-	add(pointer, value);
+	add(pointer, value);	
 	add(pointer, -value);
 }
 
 long long counter = 0;
-struct timespec time;
+struct timespec timer;
 static struct option long_options[] = {
 		{ "--threads", required_argument, 0, 'a' },
 		{ "--iter", required_argument, 0, 'b' },
@@ -35,7 +35,7 @@ int main(int argc, char** argv){
 	int threads, iterations;
 	threads = iterations = 1;
 	int i = 0;
-	struct thread_info a;
+	thread_info a;
 	a.n_count = &counter;
 	a.add_amount = 1;
 	long operations;
@@ -62,10 +62,10 @@ int main(int argc, char** argv){
 		fprintf(stderr, "Error: memory not allocated\n");
 		exit(1);
 	}
-	clock_gettime(CLOCK_MONOTONIC, time);
+	clock_gettime(CLOCK_MONOTONIC, timer);
 	time_init = time.tv_sec * 1000000000 + time.tv_nsec;
 	for (int a = 0, create_check = 0; a < threads; a++){
-		create_check = pthread_create(tids + a, 0, add, &a);
+		create_check = pthread_create(tids + a, 0, &add, &a);
 		if (create_check){
 			fprintf(stderr, "Error: unable to create thread\n");
 		}
@@ -74,7 +74,8 @@ int main(int argc, char** argv){
 		pthread_join(tids[i], 0);
 	}
 	free(tids);
-	clock_gettime(CLOCK_MONOTONIC, time);
+	free(a);
+	clock_gettime(CLOCK_MONOTONIC, timer);
 	time_finish = time.tv_sec * 1000000000 + time.tv_nsec - time_init;
 	operations = threads* iterations * 2;
 	fprintf(stdout, "%d threads x %d 10000 iterations x (add + subtract) = %l operations\n", threads, iterations, operations);
